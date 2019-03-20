aefb745c67243041b0024f123b87227d654fff40
Ting <yellowyellowting@163.com>
2019-03-20 11:06:35 +0800

    添加ID

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -19,7 +19,7 @@ exports.pop = function (req, res) {
             "des": "每年总要吃几次蛋糕啊！"
         },
         {
-            "id": "103704010",
+            "id": "103708642",
             "name": "香烤鸡翅",
             "thumbnail": "http://i2.chuimg.com/a4a8e165867d4bf486a404a879db2e9a_1539w_1154h.jpg?imageView2/1/w/215/h/136/interlace/1/q/90",
             "cookId": "120967814",
@@ -36,7 +36,7 @@ exports.pop = function (req, res) {
             "des": "喜欢吃粗娘的孩纸 肿么可以不会做面包呢~~"
         },
         {
-            "id": "102891301",
+            "id": "103708643",
             "name": "平菇青椒炒鸡蛋 好吃又营养的超快手家常菜",
             "thumbnail": "http://i2.chuimg.com/a4a8e165867d4bf486a404a879db2e9a_1539w_1154h.jpg?imageView2/1/w/215/h/136/interlace/1/q/90",
             "cookId": "122079365",
@@ -53,7 +53,7 @@ exports.pop = function (req, res) {
             "des": "我随行做，你随性看。"
         },
         {
-            "id": "102295729",
+            "id": "103708644",
             "name": "盘丝饼",
             "thumbnail": "http://i1.chuimg.com/4a30ea4e407311e7947d0242ac110002_720w_1280h.jpg@2o_50sh_1pr_1l_235w_138h_1c_1e_90q_1wh",
             "cookId": "101553997",
@@ -70,7 +70,7 @@ exports.pop = function (req, res) {
             "des": "从南到北，春天急急地赶路，到华北，已是稍纵即逝，"
         },
         {
-            "id": "102202490",
+            "id": "103708645",
             "name": "『1234糖醋排骨』",
             "thumbnail": "http://i2.chuimg.com/27591c3c296511e7bc9d0242ac110002_1324w_1984h.jpg?imageView2/1/w/235/h/138/interlace/1/q/90",
             "cookId": "107144295",
@@ -87,7 +87,7 @@ exports.pop = function (req, res) {
             "des": "是日，远远分享"
         },
         {
-            "id": "102306288",
+            "id": "103708646",
             "name": "饭店必点的干锅花菜，是这么做出来的。",
             "thumbnail": "http://i1.chuimg.com/0e8f9a644ab211e7947d0242ac110002_1000w_666h.jpg@2o_50sh_1pr_1l_235w_138h_1c_1e_90q_1wh",
             "cookId": "100351317",
@@ -104,7 +104,7 @@ exports.pop = function (req, res) {
             "des": "每张照片都有自己的故事"
         },
         {
-            "id": "100284081",
+            "id": "103708647",
             "name": "荷兰宝贝 Dutch Baby",
             "thumbnail": "http://i2.chuimg.com/9d43282c885011e6a9a10242ac110002_1800w_1200h.jpg?imageView2/1/w/235/h/138/interlace/1/q/90",
             "cookId": "10666666",
@@ -121,7 +121,7 @@ exports.pop = function (req, res) {
             "des": "每张照片都有自己的故事"
         },
         {
-            "id": "103708641",
+            "id": "103708648",
             "name": "超级绵~奶白吐司",
             "thumbnail": "http://i2.chuimg.com/dfd1c610155b47ef8418eac4db333f43_1650w_1100h.jpg?imageView2/1/w/235/h/138/interlace/1/q/90",
             "cookId": "10585157",
@@ -137,7 +137,7 @@ exports.pop = function (req, res) {
             "des": "每张照片都有自己的故事"
         },
         {
-            "id": "100232976",
+            "id": "103708649",
             "name": "《Tinrry下午茶》教你做芒果班戟和芒果千层饼",
             "thumbnail": "http://i2.chuimg.com/6d9419bc883f11e6b87c0242ac110003_578w_420h.jpg?imageView2/1/w/235/h/138/interlace/1/q/90",
             "cookId": "10411331",
@@ -154,7 +154,7 @@ exports.pop = function (req, res) {
             "des": "每张照片都有自己的故事"
         },
         {
-            "id": "103699409",
+            "id": "103708650",
             "name": "蛋糕卷，肉松蛋糕卷，网红贝贝肉松卷",
             "thumbnail": "http://i2.chuimg.com/e58cfcce29f5471d88f62bc75686dc09_1280w_854h.jpg?imageView2/1/w/235/h/138/interlace/1/q/90",
             "cookId": "126334531",
@@ -171,7 +171,7 @@ exports.pop = function (req, res) {
             "des": "每张照片都有自己的故事"
         },
         {
-            "id": "102276846",
+            "id": "103708651",
             "name": "自制肉松小贝",
             "thumbnail": "http://i2.chuimg.com/d8e9d0522f4311e7bc9d0242ac110002_1620w_1080h.jpg?imageView2/1/w/235/h/138/interlace/1/q/90",
             "cookId": "105784662",
